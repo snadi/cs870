40cccdd6f80b8e51efa17489728597c8733867de
karimhamdan@gmail.com <karimhamdan@gmail.com@b4738e21-1fe0-f5a0-35f9-4403cb083993>
2010-10-21 03:48:31 +0000

    created some dirs

--- trunk/cs870/a1/SetOperations/shapeUnion.m
+++ trunk/cs870/a1/SetOperations/shapeUnion.m
@@ -1,5 +1,4 @@
-function finalShape = shapeUnion(shape1, shape2)
-% the function returns the intersection of two shapes, i.e. min(shape1,
-% shape2)
+% the function returns the intersection of two shapes, i.e. min(shape1, shape2)
 
+function finalShape = shapeUnion(shape1, shape2)
 finalShape = min(shape1, shape2);
