cdd3d32b4f424b1097005436745c4879bd35cfc0
karimhamdan@gmail.com <karimhamdan@gmail.com@b4738e21-1fe0-f5a0-35f9-4403cb083993>
2010-10-21 03:48:31 +0000

    created some dirs

--- cs870/a1/SetOperations/shapeIntersection.m
+++ cs870/a1/SetOperations/shapeIntersection.m
@@ -1,5 +1,4 @@
-function finalShape = shapeIntersection(shape1, shape2)
-% the function returns the intersection of two shapes, i.e. max(shape1,
-% shape2)
+% the function returns the intersection of two shapes, i.e. max(shape1, shape2)
 
+function finalShape = shapeIntersection(shape1, shape2)
 finalShape = max(shape1, shape2);
