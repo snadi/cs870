fa180c46d5efe3c21645cb86d121a1a8c832daf8
karimhamdan@gmail.com <karimhamdan@gmail.com@b4738e21-1fe0-f5a0-35f9-4403cb083993>
2010-10-21 03:48:42 +0000

    created some dirs

--- cs870/a1/InitialPhi/shapeRectangle.m
+++ cs870/a1/InitialPhi/shapeRectangle.m
@@ -1,21 +1,17 @@
-function initialPhi = shapeCuboid(m)
-% shapeCone: create a level set function phi for a cuboid. Therefore, when we 
-% visualize the zero level of phi it will give us a rectangle.
+% shapeRectangle: create a level set function phi whose zero level gives
+% a rectangle.
 % 
 % Output parameters:
 %   initialPhi = the (m+2)x(m+2) matrix that holds the initial values for phi.
 % 
 % Input parameters:
-%   radius = the radius of the circle (i.e. the zero level set of phi).
-%   center = [x y] vector for the center of the circle.
 %   m = the number of cells in the grid.
 
-% h = deltaX
-h = 1 / (m + 1);
+function initialPhi = shapeRectangle(m)
 
 % Given an mxm grid we should get (m+1)x(m+1) matrix for phi. Since Matlab is 
 % 1-based, we'll create an (m+2)x(m+2) matrix for phi.
-initialPhi = ones(m+2, m+2);
+initialPhi = ones(m, m);
 
 
 %
@@ -23,8 +19,8 @@ initialPhi = ones(m+2, m+2);
 % is to count for the fact that Matlab is 1-based. So instead of having the
 % domain from (1) to (m+2), we want it to be (0) to (m+1) (given that omega
 % is defines as (0,1)x(0,1).
-for i = 1 : m + 2
-    for j = 1 : m + 2
+for i = 1 : m
+    for j = 1 : m
         initialPhi(i, j) = ;
     end
 end
