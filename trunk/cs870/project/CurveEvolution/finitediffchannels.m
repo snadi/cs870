66ce75d62f9c4557c7abbc1e6b158445f295e7d1
karimhamdan@gmail.com <karimhamdan@gmail.com@b4738e21-1fe0-f5a0-35f9-4403cb083993>
2010-11-21 23:47:26 +0000

    part 2 - kinda first draft.

--- trunk/cs870/project/CurveEvolution/finitediffchannels.m
+++ trunk/cs870/project/CurveEvolution/finitediffchannels.m
@@ -7,10 +7,14 @@ a = norm(gradient(phi));
 %calculating the curvature term in the PDE
 curvature = kappa(phi);
 
-force1 = double(fin(A1,A2,phi,logicop));
-force2 = double(fout(A1,A2,phi,logicop));
 
-phi_t =  a*(mu*curvature -lambda*(force1 + force2));
+%force1 = double(fin(A1,A2,phi,logicop));
+%force2 = double(fout(A1,A2,phi,logicop));
+force1 = fin(A1, A2, phi, logicop);
+force2 = fout(A1, A2, phi, logicop);
+
+%phi_t =  a*(mu*curvature -lambda*(force1 + force2));
+phi_t = -lambda * (force1 + force2);
 
 resultingPhi = phi + deltaT*phi_t;
 
