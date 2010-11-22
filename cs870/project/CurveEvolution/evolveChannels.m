6cf899c420c9f5c8f841d80acdcdfb092534a1be
sarah.ezznadi <sarah.ezznadi@b4738e21-1fe0-f5a0-35f9-4403cb083993>
2010-11-22 15:23:59 +0000

    updates

--- cs870/project/CurveEvolution/evolveChannels.m
+++ cs870/project/CurveEvolution/evolveChannels.m
@@ -3,13 +3,15 @@ function evolveChannels(iterations)
 plotStep = 1;               
 t0 = 0;                      % Start at time t = 0
 
-image2 = rgb2gray(imread('brain1.jpg'));
-image1 = rgb2gray(imread('brain3.jpg'));
-objective = rgb2gray(imread('brain.jpg'));
+image2 = rgb2gray(imread('data/donuttopright.jpg'));
+image1 = rgb2gray(imread('data/donutbottomleft.jpg'));
+objective = rgb2gray(imread('data/donut.jpg'));
+
+display('in evolve channels');
 
 
 grid = constructGrid(size(image1,1));
-phi0 =  cone(20, [50 50], 131);
+phi0 =  cone(50, [80 80], 300);
 
 figure();
 subplot(2,3,1); imshow(image1); title('Input Image 1');
@@ -25,9 +27,9 @@ phi = phi0;
 %---------------------------------------------------------------------------
 % Evolve the curve
 t = t0;
-deltaT = 1.5;% (grid.upperRightCorner(1) - grid.lowerLeftCorner(1)) / grid.gridSize;
+deltaT = 0.05;% (grid.upperRightCorner(1) - grid.lowerLeftCorner(1)) / grid.gridSize;
 
-        
+        display('starting iterations');
 for n=1:iterations
     if(mod(n, plotStep) == 0) 
         subplot(2,3,4);
@@ -38,8 +40,8 @@ for n=1:iterations
         seg = phi>=0;
         subplot(2,3,5); imshow(seg); title('Segmentation');             
     end
-    lambda = (max(max(image1)))^2;
-    phi_new = finitediffchannels(image1,image2,phi,deltaT, 0.01, lambda, 'union');
+    lambda = 100;
+    phi_new = finitediffchannels(image1,image2,phi,deltaT, 0.01, lambda, 'intersection');
     
     phi = phi_new;
     t = t + deltaT;      
