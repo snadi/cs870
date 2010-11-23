fce3c372ed4e8d96eb65e3798f799d067f80de09
karimhamdan@gmail.com <karimhamdan@gmail.com@b4738e21-1fe0-f5a0-35f9-4403cb083993>
2010-11-23 20:15:09 +0000

    intersection resolving conflict

--- cs870/project/logic-operations/scintersection.m
+++ cs870/project/logic-operations/scintersection.m
@@ -2,12 +2,12 @@ function output = scintersection(A1, A2, phi, region)
 
 switch region
     case 'inside'      
-        output = 1 - ((1 - zin(A1, phi)).*(1-zin(A2,phi))).^0.5;               
+        output = 1 - ((1 - zin(A1, phi)).*(1-zin(A2,phi))).^0.5;
     case 'outside'       
-        output = 1 - ((1 - zout(A1, phi)).*(1-zout(A2,phi))).^0.5;        
+        output = 1 - ((1 - zout(A1, phi)).*(1-zout(A2,phi))).^0.5;
 end
 
-%output = real(output);
+output = real(output);
 
 end
 
