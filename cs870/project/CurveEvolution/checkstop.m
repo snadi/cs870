b65ee5a2c9ba7c042aa40da39a7a6b526be74e8e
sarah.ezznadi <sarah.ezznadi@b4738e21-1fe0-f5a0-35f9-4403cb083993>
2010-12-01 15:20:39 +0000

    stopping condition should be working

--- cs870/project/CurveEvolution/checkstop.m
+++ cs870/project/CurveEvolution/checkstop.m
@@ -1,11 +1,19 @@
-function indicator = checkstop(old, new)
+function indicator = checkstop(image, old, new)
 
-error = abs( (old - new) ./ old);
-error(~isfinite(error)) = 0;   % Sets Inf and NaN to 0
+% error = abs( (old - new) ./ old);
+% error(~isfinite(error)) = 0;   % Sets Inf and NaN to 0
+% 
+% errorValue = max(max(error));
+% display(errorValue);
 
-display(max(max(error)));
-pause;
+error = size(image(old >= 0), 1) - size(image(new >=0), 1);
 
-indicator = 0;
+
+
+if(error == 0)
+	indicator = 1;
+else
+	indicator = 0;
+end;
 
 end
