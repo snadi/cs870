52f96d0934a7915b0045a853411a37077a67beaf
karimhamdan@gmail.com <karimhamdan@gmail.com@b4738e21-1fe0-f5a0-35f9-4403cb083993>
2010-12-01 22:40:37 +0000

    code cleanup, fadel test, evolveChannels, Logic, InitialPhi

--- trunk/cs870/project/CurveEvolution/reinit.m
+++ trunk/cs870/project/CurveEvolution/reinit.m
@@ -1,5 +1,12 @@
+% reinit: reinitialize phi
+%
+% Output parameters:
+%   signedPhi = reinitialized SDF phi
+%
+% Input parameters:
+%   phi = current value of phi to be reinitialized
+
 function signedPhi = reinit(phi)
 
+% Per our class notes.
 signedPhi = phi ./ sqrt(phi.^2 + 1);
-
-end
--- trunk/cs870/project/CurveEvolution/stop.m
+++ trunk/cs870/project/CurveEvolution/stop.m
@@ -1,13 +1,17 @@
-function indicator = checkstop(image, old, new)
+% stop: checks whether phi is stationary or not
+%
+% Output parameters:
+%   indicator = stop or not ?
+%
+% Input parameters:
+%   image = the original image
+%   old = old phi
+%   new = new phi
 
+function indicator = stop(image, old, new)
+
+% Basically, what we're trying to do here is to detect if the new phi
+% introduces new points inside the curve or not.
 error = size(image(old >= 0), 1) - size(image(new >=0), 1);
 
-
-
-if(error == 0)
-	indicator = 1;
-else
-	indicator = 0;
-end;
-
-end
+indicator = vif(error == 0, 1, 0);
