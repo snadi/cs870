56a4b5cd28b412df358a56b187089aab82d6b966
karimhamdan@gmail.com <karimhamdan@gmail.com@b4738e21-1fe0-f5a0-35f9-4403cb083993>
2010-12-02 21:26:47 +0000

    cleaned up cone

--- trunk/cs870/project/InitialPhi/cone.m
+++ trunk/cs870/project/InitialPhi/cone.m
@@ -1,17 +1,24 @@
-%Sarah Nadi 20290927
+% cone: create a level set function phi for a cone. Therefore, when we 
+% visualize the zero level of phi it will give us a circle.
+% 
+% Output parameters:
+%   phi = the level set function for a cirlce given in matrix form
+% 
+% Input parameters:
+%   radius = the radius of the circle (i.e. the zero level set of phi).
+%   center = [x y] vector for the center of the circle.
+%   imagedDims = the grid that will be used for approximation.
 
-%this constructs a cone with the equation given in the paper  to make sure
-%that inside the cirlce is positive and outside is negative
-function phi = cone(radius, center, m)
-h = 1 / (m - 1);
+function phi = cone(radius, center, imageDims)
 
-phi = zeros(m,m);
-for i = 1 : m 
-    for j = 1 : m
-        phi(i,j) = -1*(sqrt( ((j-1) - center(1))^2 + ((i-1) - center(2))^2)) + radius;
-    end
-end
-end
+% Since we're allowed to analytically initialize phi, we can use the equation 
+% given in class to fill out the matrix.
+% General formula => phi = sqrt((x - centerX)^2 + (y - centerY)^2) - radius
+xy = cell(2, 1);
+xy{1} = 1 : 1 : imageDims(1);
+xy{2} = 1 : 1 : imageDims(2);
 
+axes = cell(2, 1);
+[ axes{:} ] = ndgrid(xy);
 
-
+phi = sqrt( (axes{1} - center(1)).^2 + (axes{2} - center(2)).^2 ) - radius;
