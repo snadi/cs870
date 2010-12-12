fdb370a43d5d9feec36638933d4e5978a74da1b0
karimhamdan@gmail.com <karimhamdan@gmail.com@b4738e21-1fe0-f5a0-35f9-4403cb083993>
2010-12-12 14:37:23 +0000

    tests

--- trunk/cs870/project/Tests/test2.m
+++ trunk/cs870/project/Tests/test2.m
@@ -1,19 +1,18 @@
 % test2: testing the implementation of the Sandberg-Chan model
-
 function test2()
 
 startTime = cputime;
 
 iterations = 10;
 
-% image = imread('flowers.jpg');
-image = readGSImage('image.jpg');
+image = imread('flowers.jpg');
+% image = readGSImage('image.jpg');
 imageSize = size(image, 1);
 
-% [r g b] = readRGBImage('flowers.jpg');
+[r g b] = readRGBImage('flowers.jpg');
 
-image1 = readGSImage('image1.jpg');
-image2 = readGSImage('image2.jpg');
+% image1 = readGSImage('image1.jpg');
+% image2 = readGSImage('image2.jpg');
 % image3 = readGSImage('image3.jpg');
 
 phi0 = cone(imageSize/4, [imageSize/2 imageSize/2], size(image));
@@ -22,14 +21,14 @@ mu = 0.01;
 
 lambda = 255*255;
 
-logicop = 'intersection';
+logicop = 'union';
 
 doReinit = false;
 
-complement = 2;
+complement = 0;
 
-evolveChannels(iterations, phi0, mu, lambda, logicop, image, doReinit, complement, image1, image2);
-% evolveChannels(iterations, phi0, mu, lambda, logicop, image, doReinit, r, g, b);
+% evolveChannels(iterations, phi0, mu, lambda, logicop, image, doReinit, complement, image1, image2, image3);
+evolveChannels(iterations, phi0, mu, lambda, logicop, image, doReinit, complement, r, g, b);
 
 timeTaken = cputime - startTime;
 
